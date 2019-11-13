c290cd077f985ef12733035e99a6dd8abc4df390
MiquelRentero <57458124+MiquelRentero@users.noreply.github.com>
2019-11-13 18:18:03 +0100

    Update practica15MP.sh

--- practica15MP.sh
+++ practica15MP.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # 1-Assegurate de que l'script se ejecuta con privilegios de superusuario.
 if [[ "${UID}" -eq 0 ]]
 then
@@ -17,5 +18,28 @@ echo "Introduzca la contraseña: "
 read -s password
 # 5-Crear el usuario.
 useradd -c "${nomreal}" -m ${nombre}
+# 6-Comprovar si el comando "useradd" se ha producido correctamente.
+if [ $? -eq 0 ]
+then
+  echo "Se ha creado correctamente"
+else
+  echo "No se ha podido crear"
+  exit 1
+fi
+# 7-Cambiar la contraseña.
+echo ${nombre}":"${password} | chpasswd
+# 8-Comprovar si el comando "passwd" se ha producido correctamente.
+if [ $? -eq 0 ]
+then
+  echo "Se ha asignado correctamente"
+else
+  echo "No se ha podido asignar"
+  exit 1
+fi
+# 9-Forzar el cambio de contraseña en el primer login.
+echo "Es obligado cambiar la contraseña la primera vez que entras con este usuraio"
+read -s password2
+echo ${nombre}":"${password2} | chpasswd
+echo ${password2} | passwd ${nombre}
 # 10-Mostrar el nombre de usuario, la contraseña y el host donde se ha creado el usuario.
 echo "Tu nombre de usuario es: ${nombre}, tu contraseña es: ${password2} y el host desde el que lo has creado es: ${HOSTNAME}"
